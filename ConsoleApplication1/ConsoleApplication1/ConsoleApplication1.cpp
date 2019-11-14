140af12c1af6a833c30b31fa13a739e433ce4cd6
Logan Kilby <logankilby@gmail.com>
2019-11-13 23:29:57 -0600

    player_map working and writing to file

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,8 +1,5 @@
 #include "Functions.h"
 
-//no longer writes to txt-file. pushes to map player_map. will add file writing next - 11/13/2019
-
-
 int main()
 {
 	int choice;
@@ -26,20 +23,33 @@ int main()
 			if (output_file.is_open()) {
 				do {
 					cout << "\nTo return to the menu, enter the symbol ^ : " << endl;
-					cout << "\t" << "Enter the player's first and last name: ";
+					cout << '\t' << "Enter the player's first and last name: ";
 					getline(cin, new_player_name);
 				
 					if (new_player_name == "^") {
-						cout << '\n' << player_map.size() << " player(s) have been added";
+						cout << '\n' << player_map.size() << " player(s) have been added:" << endl;
+						for (auto const& x : player_map) { 
+							cout << x.first << " : " << x.second << endl;
+							output_file << x.first << " : " << x.second << endl; // @ref this output should probably be formated for reading from file
+						}
+
 						output_file.close();
 
 					} else {
-						cout << "\t" << "Enter the player's jersey number: ";
+						cout << '\t' << "Enter the player's jersey number: ";
 						cin >> new_player_number;
 						cin.clear();
 						cin.ignore(1000, '\n');
 
-						player_map.insert({new_player_name, new_player_number});
+						pair<map<string, int>::iterator, bool> checker; //using the built-in boolean return value to check for insert failure
+						checker = player_map.insert(pair<string, int>(new_player_name, new_player_number));
+						if (checker.second == false) {
+							cout << "Error. This player has already been entered." << endl;
+
+						} else {
+							player_map.insert(pair<string, int>(new_player_name, new_player_number));
+
+						}
 
 					}
 
@@ -62,8 +72,11 @@ int main()
 
 		default:
 			cout << "Please choose a number from the given list.";
+
 		}
+
 	} while (choice != 9);
+
 }
 
 
